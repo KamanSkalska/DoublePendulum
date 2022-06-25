ed03fec56695ff763cc1e13f3c700d20fedf7ab4
juziembl <01169369@pw.edu.pl>
2022-06-25 19:36:54 +0200

    fixed plot issues

--- anglesPlot.py
+++ anglesPlot.py
@@ -2,15 +2,15 @@ import math
 import matplotlib.pyplot as plt
 
 
+def float_pi_mod(num: float):
+    return math.asin(math.sin(num))
+
+
 def drawing_plots(angle1, angle2, fps):
     t = list(range(0, len(angle1)))
     for i in range(len(angle1)):
-        if angle1[i] > 10*math.pi or angle1[i] < -10*math.pi:
-            angle1[i] %= 2*math.pi
-        if angle2[i] > 10*math.pi or angle2[i] < -10*math.pi:
-            angle2[i] %= 2*math.pi
-        angle1[i] /= math.pi
-        angle2[i] /= math.pi
+        angle1[i] = float_pi_mod(angle1[i])
+        angle2[i] = float_pi_mod(angle2[i])
     t = [i / fps for i in t]
     plt.plot(t, angle1, label="Kąt 1")
     plt.plot(t, angle2, label="Kąt 2")
